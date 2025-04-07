4a350f16bdee55dd93b9ce62a777702b4cff4717
HarrisonGuerrero18 <hguerreropalacios17@gmail.com>
2025-04-08 02:55:47 +1200

    Create ciclo_for.py

---
+++ ciclo_for.py
@@ -0,0 +1,24 @@
+# Colección de datos de Ejemplo:
+# ¿Qué es una lista?: es un conjunto de daos simples.
+# - Se encierran entre brackets
+# - Se se separan por comas
+
+lista = [
+        "Apple",
+        "Orange",
+        "Grape",
+        "Mango",
+        "Strawberry",
+        "Pineapple",
+        "Watermelon",
+        "Tomato"
+        ]       
+print(lista)
+
+# Iteramos la lista con un ciclo for
+
+# variable de almacenamiento:
+# singular de tipo de objetos
+# que contiene la lista
+for fruta in lista:   
+    print(fruta) # Imprime solo los últimos 2 galores de la lista
